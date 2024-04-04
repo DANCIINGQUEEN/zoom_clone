8f7e57a50fdbe16dd063f8e8c80501c3cb1528ac
park <park>
2024-04-04 19:04:45 +0900

    Add new dependencies and components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
-
+import '@stream-io/video-react-sdk/dist/css/styles.css';
+import { Toaster } from "@/components/ui/toaster" 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -30,7 +31,10 @@ export default function RootLayout({
           colorInputText:'#fff'
         }
       }}>
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster/>
+          </body>
       </ClerkProvider>
     </html>
   )
